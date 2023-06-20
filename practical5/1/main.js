de80b1d9c6e1c45e6fb44790fde77c9feb70400d
Olesia Bosa <olesia.olecsandrova@gmail.com>
2023-06-20 21:24:21 +0300

    first commit

---
+++ practical5/1/main.js
@@ -0,0 +1,30 @@
+var name = prompt('Input name','');
+var position = prompt('Input position','');
+var phone = prompt('Input phone', '');
+
+var visitcard = ' <TABLE width="100%" border="0">'
++' <TR>'
++' <TD align="CENTER" valign="CENTER">'
++' <IMG src="images/visit_card.gif">'
++' </TD>'
++' <TD align="CENTER">'
++' <B>' + name + '</B><BR>'
++' <I>' + position + '</I><BR>'
++' <FONT color="blue">' + phone + '</FONT>'
++' </TD>'
++' </TR>'
++' </TABLE>';
+
+document.write('<TABLE width="100%" border="0" align="CENTER">');
+for(var y=0; y<12; y++)
+{
+document.write('<TR>');
+for(var x=0; x<3; x++)
+{
+document.write( '<TD align="CENTER" style="border-style: dashed; borderwidth:thin;">');
+document.write( visitcard );
+document.write('</TD>');
+}
+document.write('</TR>');
+}
+document.write('</TABLE>'); 
---
+++ practical6/3/script.js
@@ -0,0 +1,9 @@
+function checkAnswer(answer) {
+    var correctAnswer = "9";
+  
+    if (answer === correctAnswer) {
+      alert("Відповідь правильна!");
+    } else {
+      alert("Відповідь неправильна. Спробуйте ще раз.");
+    }
+  }
